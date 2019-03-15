71355ee6a6ddfef156b9c395e1583bf5439bb2ad
Florin <oancea.quentin.florin@gmail.com>
2019-03-15 20:35:55 +0200

    solution

--- product_except_at_index/script.js
+++ product_except_at_index/script.js
@@ -3,6 +3,24 @@ const getProductsOfAllElementsExcetpAtIndex = (arr) => {
         console.log("not enough elements");
         return false;
     }
+    let listOfProducts = arr.map(el => 0);
+
+    let productNow = 1;
+    arr.map((el, i) => {
+        listOfProducts[i] = productNow;
+        productNow *= arr[i];
+    });
+
+    console.log(`list or the products of all the previous elements ${listOfProducts}`);
+
+    productNow = 1;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        listOfProducts[i] *= productNow;
+        productNow *= arr[i];
+    }
+    
+    console.log(`list or the products of all the previous and next elements ${listOfProducts}`);
 
 };
-getProductsOfAllElementsExcetpAtIndex([1, 2]);
+
+getProductsOfAllElementsExcetpAtIndex([1, 2, 6, 5, 9]);
