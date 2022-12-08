f94c5a666bf1b01741e853508dcd426183e5c593
Florin <florin.oancea@arxall.com>
2022-12-08 16:23:31 +0200

    hash table

---
+++ hash-tables/first_recurring_character.js
@@ -0,0 +1,34 @@
+/**
+ * Given an array write a function that returns the first recurring character
+ * ex: [2,5,1,2,3,4,5,6,3,5] // 2
+ * ex: [2,1,1,1,1,1,1,1,1,4,4,,4,4,1,123,41,234,123] // 2
+ * ex: [12,2,3,4] // undefined
+ * */
+
+//naive approach O(n^2)
+const firstRecurringCharacter = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                return arr[i]
+            }
+        }
+    }
+    return undefined
+}
+
+// console.log(firstRecurringCharacter([2, 5, 5, 2, 5, 3])) // 2
+
+function firstRecurringCharacter2(arr) {
+    const map = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (map[arr[i]]) {
+            return arr[i]
+        } else {
+            map[arr[i]] = true;
+        }
+    }
+    return undefined;
+}
+
+console.log(firstRecurringCharacter2([2, 3, 5, 5, 2, 3]))// 2 O(n) - increasing the space complexity, e have a new variable
